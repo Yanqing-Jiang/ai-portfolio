2ddc8323cc5296dfdda928d434a72e3ae2c3fe08
Yanqing-Jiang <jiangyanqing90@gmail.com>
2025-12-30 00:40:56 -0500

    Update generative UI widget registry

--- components/generativeUiDashboard/renderer/widgets/index.ts
+++ components/generativeUiDashboard/renderer/widgets/index.ts
@@ -8,4 +8,5 @@ export { DataTable } from './DataTable';
 export { NewsTimeline } from './NewsTimeline';
 export { CorrelationMatrix } from './CorrelationMatrix';
 export { ExplainMovePanel } from './ExplainMovePanel';
+export { ErrorPanel } from './ErrorPanel';
 
